35341957f1836c7d01c9d9c506e04497ade45ef2
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2025-08-29 23:54:22 +0100

    Adds Eze Brokerage in CLI

--- 01 Cloud Platform/10 Live Trading/02 Brokerages/15 SSC Eze/14 Deploy Live Algorithms.php
+++ 01 Cloud Platform/10 Live Trading/02 Brokerages/15 SSC Eze/14 Deploy Live Algorithms.php
@@ -3,7 +3,7 @@ $brokerageName = "SS&C Eze";
 $cashState = false;
 $holdingsState = false;
 $secondBullet = "";
-$authentication = "";
+$authentication = file_get_contents(DOCS_RESOURCES."/brokerages/eze/deploy-steps.php");
 $postDeploy = "";
 $dataProviderDetails = "<p>In most cases, we suggest using the <a href='/docs/v2/cloud-platform/datasets'>QuantConnect data provider</a>, the <a href='/docs/v2/cloud-platform/datasets/ssc-eze'>SS&C Eze data provider</a>, or both. The order you set them in the deployment wizard defines their order of precedence in Lean.</p>";
 include(DOCS_RESOURCES."/live-trading/deploy-live-algorithm.php");
